e38646899750674abef839fe1d5127605230cbde
ChokshiUtsav <uvc.smart.29@gmail.com>
2016-09-02 01:39:51 +0530

    Added general resources

---
+++ template.cpp
@@ -0,0 +1,36 @@
+#include<bits/stdc++.h>
+#define s(n)                        scanf("%d",&n)
+#define sc(n)                       scanf("%c",&n)
+#define sl(n)                       scanf("%lld",&n)
+#define sf(n)                       scanf("%lf",&n)
+#define ss(n)                       scanf("%s",n)
+#define fill(a,v)                    memset(a, v, sizeof a)
+#define sz(a)                       sizeof(a)/sizeof()
+
+// Some common useful functions
+#define maxi(a,b)                     ( (a) > (b) ? (a) : (b))
+#define mini(a,b)                     ( (a) < (b) ? (a) : (b))
+#define checkbit(n,b)                ( (n >> b) & 1)
+#define INDEX(arr,ind)               (lower_bound(all(arr),ind)-arr.begin())
+
+#define mod 1000000007
+#define int_inf  1e8
+#define long_inf 1e18
+
+#define fr freopen("B-large.in","r",stdin)
+#define fo freopen("output.txt","w",stdout)
+#define print1(cnt,x) cout<<"Case #"<<cnt++<<": "<<x<<"\n";   // single-line o/p testcases
+#define print2(cnt) cout<<"Case #"<<cnt++<<":"<<"\n";         // multiple-line o/p testcases
+#define sit set<ull>::iterator
+#define mit map<ull,ull>::iterator
+
+using namespace std;
+
+typedef unsigned int ui;
+typedef long long ll;
+typedef unsigned long long ull;
+typedef long double ld;
+
+int main(){	
+	return 0;
+}
